2a5feed88a8e3c2e72a16431615a9034e7efbf20
soyeon1351 <pack6411351@gmail.com>
2025-04-18 02:34:06 +0900

    ✨ Viewer 컴포넌트 생성
    카운트 값을 출력하는 뷰어 컴포넌트 생성 및 기본 레이아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import React from 'react'
 import './App.css'
+import Viewer from "./component/Viewer"
 
 function App() {
-
+  return(
+    <div className='App'>
+  <Viewer />
+    </div>
+  )
 }
 
 export default App
---
+++ src/component/Viewer.jsx
@@ -0,0 +1,11 @@
+const Viewer = () => {
+    return (
+        <div>
+            <h1>SIMPLE CONUNTER</h1>
+            <p>현재 카운트:</p>
+            <p>0</p>
+        </div>
+    )
+}
+
+export default Viewer;
